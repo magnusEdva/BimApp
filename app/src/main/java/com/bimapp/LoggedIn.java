2f6944be897903c73519722e8f7b90357634c46b
Håkon Eriksen <eriksen.hakon@gmail.com>
2018-03-13 13:17:57 +0100

    Merge branch 'master' of https://github.com/magnusEdva/BimApp
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/bimapp/LoggedIn.java
+++ app/src/main/java/com/bimapp/LoggedIn.java
@@ -6,24 +6,90 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoggedIn extends AppCompatActivity {
+
+    BimApp mApplication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
-
+        mApplication = (BimApp) this.getApplicationContext();
         Uri uri = getIntent().getData();
 
         if (uri != null && uri.toString().startsWith("bimapp://oauthresponse")) {
             storeCode(uri.getQueryParameter("code"));
+            Log.d(uri.toString(), "message");
         }
+        String url = "https://api.bimsync.com/oauth2/token"; //"http://10.0.0.8:8089/oauth2/token"
 
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            Log.d("Access Token","???");
+                        } catch (Exception e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Something happened", error.toString());
+                        error.printStackTrace();
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<>();
+                // the POST parameters:
+                params.put("client_id", APIkey.Client_id);
+                params.put("client_secret",APIkey.Secret_id);
+                params.put("code", mApplication.getAuthorizatonCode() );
+                params.put("grant_type", "authorization_code");
+                params.put("redirect_uri", "bimapp://oauthresponse");
+                return params;
+            }
+            @Override
+                    public String getBodyContentType()
+            {
+                return "application/x-www-form-urlencoded";
+
+            }
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError
+            {
+                Map<String, String> headers = new HashMap<String, String>();
+
+                headers.put("Accept", "application/json");
+
+                return headers;
+            }
+        };
+        Volley.newRequestQueue(this).add(postRequest);
     }
 
     private void storeCode(String code) {
-        BimApp context = (BimApp) this.getApplicationContext();
-        context.setAuthorizationCode(code);
+        mApplication.setAuthorizationCode(code);
     }
 
 
--- app/src/main/java/com/bimapp/model/oauth/OAuthHandler.java
+++ app/src/main/java/com/bimapp/model/oauth/OAuthHandler.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.support.customtabs.CustomTabsIntent;
 
+import com.bimapp.APIkey;
 import com.bimapp.BimApp;
 import com.bimapp.R;
 
@@ -31,11 +32,12 @@ public class OAuthHandler {
 
     public String getoAuthUrI(){
         StringBuilder URI = new StringBuilder();
-        URI.append(mContext.getText(R.string.BimSyncURL));
-        URI.append(mContext.getText(R.string.APIversion));
+        URI.append(mContext.getText(R.string.BimSyncURL)); //mContext.getText(R.string.BimSyncURL)"http://10.0.0.8:8089/"
         URI.append(mContext.getText(R.string.oAuth0));
         URI.append(mContext.getText(R.string.oAuthClientId));
+        URI.append(APIkey.Client_id);
         URI.append(mContext.getText(R.string.oAuthRedirectURI));
+
         URI.append(mContext.getText(R.string.oAuthEnd));
         return URI.toString();
     }
