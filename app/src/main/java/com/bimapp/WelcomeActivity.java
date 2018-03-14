ef526002a95f549c8fc0c5ef2807bb92d996ef98
Håkon Eriksen <eriksen.hakon@gmail.com>
2018-03-14 11:30:07 +0100

    Merge branch 'master' of https://github.com/magnusEdva/BimApp
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/bimapp/WelcomeActivity.java
+++ app/src/main/java/com/bimapp/WelcomeActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.bimapp.model.oauth.OAuthHandler;
 
@@ -19,20 +20,22 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
 
+    public void buttonOnClick(View v){
         mApplication = (BimApp) this.getApplicationContext();
         mApplication.getMOAuth().launchBrowser();
-
+        if(getIntent().getData() != null) {
+            String code = getIntent().getData().getQueryParameter("code");
+            mApplication.refreshToken(code, OAuthHandler.GRANT_TYPE_AUTHORIZATION_CODE);
+            Intent intent = new Intent(this, ProjectsViewActivity.class);
+            startActivity(intent);
+        }
     }
 
     @Override
     protected void onResume(){
         super.onResume();
-        if(getIntent().getData() != null) {
-            String code = getIntent().getData().getQueryParameter("code");
-            mApplication.refreshToken(code, OAuthHandler.GRANT_TYPE_AUTHORIZATION_CODE);
-            Intent intent = new Intent(this, LoggedIn.class);
-            startActivity(intent);
-        }
+
     }
 }
