a5fd0ee347ecbe804be54d293c4106ad780cf44f
Hakon <Hakon@GONGON-DESKTOP>
2018-03-22 17:13:18 +0100

    Added most things that needs to be in this class-
    Still needs an actual view, and the interface that belongs to it.

--- app/src/main/java/com/bimapp/controller/FragmentTopic.java
+++ app/src/main/java/com/bimapp/controller/FragmentTopic.java
@@ -1,26 +1,37 @@
 package com.bimapp.controller;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bimapp.BimApp;
 import com.bimapp.R;
+import com.bimapp.controller.interfaces.TopicsFragmentInterface;
+import com.bimapp.model.entity.Topic;
+import com.bimapp.model.entityManagers.TopicsEntityManager;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link FragmentTopic#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmentTopic extends Fragment {
+public class FragmentTopic extends Fragment implements TopicsFragmentInterface {
 
+    // Interface for the view, setListener makes this the callback.
+    // private TopicViewInterface mTopicViewInterface;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // Used to get topics from the model. Could potentially be implemented in this class,
+    // but probably better to separate them.
+    private TopicsEntityManager mTopicsEntityManager;
 
+    // Getting the application
+    private BimApp mApplication;
 
     public FragmentTopic() {
         // Required empty public constructor
@@ -41,13 +52,45 @@ public class FragmentTopic extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mApplication = (BimApp) this.getActivity().getApplication();
+        mTopicsEntityManager = new TopicsEntityManager(mApplication);
+        mTopicsEntityManager.getTopics(this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Instatiate the view
+        // mTopicsView = new TopicsView(inflater, container);
+        // Set this as the callback from the view
+        // mTopicsView.registerListener(this);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fragment_topic, container, false);
     }
 
+    @Override
+    public void onAttach(Context context){
+        // Doesn't do anything at the moment, but we might find uses for it!
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach(){
+        super.onDetach();
+        mTopicsEntityManager = null; // I think this helps prevent memory leaks
+    }
+
+    /**
+     * From TopicsFragmentsInterface.
+     * Calls on a method in the view and gives the topics the view should show.
+     * @param topics the topics to be listed
+     */
+    @Override
+    public void setTopics(List<Topic> topics) {
+        // This method shall do a callback to the view when it has recieved topics
+        // mTopicsView.setTopics(topics);
+    }
+
+    // Implement override method from the view that tells this controller which topic was selected
 }
