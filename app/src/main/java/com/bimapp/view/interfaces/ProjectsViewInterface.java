9abe25851e92f85ba4916d88df52514598de36e0
Hakon <Hakon@GONGON-DESKTOP>
2018-03-22 17:36:27 +0100

    Added comments.

--- app/src/main/java/com/bimapp/view/interfaces/ProjectsViewInterface.java
+++ app/src/main/java/com/bimapp/view/interfaces/ProjectsViewInterface.java
@@ -5,19 +5,23 @@ import com.bimapp.model.entity.Project;
 import java.util.List;
 
 /**
- * Created by Håkon on 20.03.2018.
+ * Interface defining the contracts between the View and the Presenter.
+ * <p>
+ * The {@link ProjectsViewInterface} interface is implemented by the Presenter, and is called from the View
+ * when something is selected by the view.
+ * <p>
+ * The other methods are implemented by the View.
+ * The {@link registerListener} method lets the View know which method to callback with {@link onSelectedItem}.
+ * The {@link setProjects} method is called by the Presenter when it has received a list of projects.
  */
 
 public interface ProjectsViewInterface extends ViewMVP {
 
-    interface ShowProjectsViewListener{
+    interface ShowProjectsViewListener {
         void onSelectedItem(Project project);
     }
 
     void registerListener(ShowProjectsViewListener projectsView);
-
     void unregisterListener();
-
     void setProjects(List<Project> projects);
-
 }
