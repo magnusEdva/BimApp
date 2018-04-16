0ac66c5bbba6b61c6c889a92376ab79d56f8ae88
Håkon Eriksen <eriksen.hakon@gmail.com>
2018-04-16 12:05:09 +0200

    Made IssueBoardExtensionsEntityManage

---
+++ app/src/main/java/com/bimapp/model/entityManagers/IssueBoardExtensionsEntityManager.java
@@ -0,0 +1,69 @@
+package com.bimapp.model.entityManagers;
+
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.bimapp.BimApp;
+import com.bimapp.model.entity.IssueBoardExtensions;
+import com.bimapp.model.entity.Project;
+import com.bimapp.model.network.APICall;
+import com.bimapp.model.network.Callback;
+import com.bimapp.model.network.NetworkConnManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class IssueBoardExtensionsEntityManager {
+
+    public interface IssueBoardExtensionsProjectCallback {
+        public interface IssueBoardExtensionsProjectListener{
+            public void getExtensions(IssueBoardExtensionsProjectCallback callback);
+        }
+        public void onGetExtensions(IssueBoardExtensions issueBoardExtensions);
+
+    }
+
+    private BimApp mContext;
+
+    public IssueBoardExtensionsEntityManager(BimApp mContext) {
+        this.mContext = mContext;
+    }
+
+    /**
+     * Inner lass which implements Callback from {@link com.bimapp.model.network.NetworkConnManager}.
+     */
+    public class IssueBoardExtensionsCallback implements Callback{
+
+        IssueBoardExtensionsProjectCallback mProjectCallback;
+        public IssueBoardExtensionsCallback(IssueBoardExtensionsProjectCallback issueBoardExtensionsCallback){
+            mProjectCallback = issueBoardExtensionsCallback;
+        }
+
+        @Override
+        public void onError(String response) {
+            Log.d("IssueBoardExtensions", "Error on getting IssueBoardExtensions.\n"
+                    + response);
+        }
+
+        @Override
+        public void onSuccess(String JSONResponse) {
+            Log.d("IssueBoardExtensions", "Successfully got IssueBoardExtensions." );
+            IssueBoardExtensions issueBoardExtensions = null;
+            try {
+                JSONObject jsonObject = new JSONObject(JSONResponse);
+                issueBoardExtensions.construct(jsonObject);
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+            mProjectCallback.onGetExtensions(issueBoardExtensions);
+        }
+    }
+
+    public void getIssueBoardExtensions(Project project, IssueBoardExtensionsCallback controllerCallback){
+        NetworkConnManager.networkRequest(mContext
+                , Request.Method.GET
+                , APICall.GETIssueBoardExtensions(project)
+                , controllerCallback
+                ,null);
+    }
+}
