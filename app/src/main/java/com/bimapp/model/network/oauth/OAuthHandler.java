388b2ea507a136ec5d11c0e1f0c767b5b6eab13f
Håkon Eriksen <eriksen.hakon@gmail.com>
2018-04-26 09:18:24 +0200

    Added TODO for adding auth urls from public resource instead of hard-coded

--- app/src/main/java/com/bimapp/model/network/oauth/OAuthHandler.java
+++ app/src/main/java/com/bimapp/model/network/oauth/OAuthHandler.java
@@ -7,7 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.customtabs.CustomTabsIntent;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
@@ -106,6 +105,7 @@ public class OAuthHandler {
             return;
         setActiveRefresh();
 
+        //TODO auth_url and token_url can be gotten from GET /bcf/{version}/auth
         String url = mContext.getString(R.string.api_token);
         StringRequest oAuthRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
