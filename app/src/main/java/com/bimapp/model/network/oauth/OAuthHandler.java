b5e1f9730f30095e91fca4d75b81d5af46751b24
Håkon Eriksen <eriksen.hakon@gmail.com>
2018-04-26 09:15:17 +0200

    Added comment to get auth urls from API instead of hardcoded

--- app/src/main/java/com/bimapp/model/network/oauth/OAuthHandler.java
+++ app/src/main/java/com/bimapp/model/network/oauth/OAuthHandler.java
@@ -7,7 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.customtabs.CustomTabsIntent;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
@@ -106,6 +105,8 @@ public class OAuthHandler {
         setActiveRefresh();
 
 
+        // url can be gotten from bcf/auth ish
+        //TODO get the string url from the {GET /bcf/{version}/auth} resource
         String url = mContext.getString(R.string.api_token);
         StringRequest oAuthRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
