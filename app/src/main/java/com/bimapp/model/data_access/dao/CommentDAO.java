7805a15135438f36d0e77b8fab6fc6cbda2761b9
magnusEdva <mlyngsedva@gmail.com>
2018-04-27 20:18:31 +0200

    Comments stored offline. Comments retrieved from offline.
    Also added logout support for the database.

--- app/src/main/java/com/bimapp/model/data_access/dao/CommentDAO.java
+++ app/src/main/java/com/bimapp/model/data_access/dao/CommentDAO.java
@@ -4,6 +4,8 @@ import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
+import android.content.ContentValues;
+import android.database.Cursor;
 
 import com.bimapp.model.entity.Comment;
 
@@ -13,10 +15,11 @@ import java.util.List;
 public interface CommentDAO {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insertAll(List<Comment> comments);
+    void insertAll(Comment ... comments);
 
     @Query("SELECT * FROM comment WHERE topic_guid = :topicGUID")
-    List<Comment> getTopicsComments(String topicGUID);
+    Cursor getTopicsComments(String topicGUID);
 
-
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Comment comment);
 }
