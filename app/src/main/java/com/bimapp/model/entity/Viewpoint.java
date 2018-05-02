59b77f89cfa19d388a0661eba542bcd05b78b600
magnusEdva <mlyngsedva@gmail.com>
2018-05-02 13:23:02 +0200

    fixed issues with snapshot Null pointers

--- app/src/main/java/com/bimapp/model/entity/Viewpoint.java
+++ app/src/main/java/com/bimapp/model/entity/Viewpoint.java
@@ -76,6 +76,7 @@ public class Viewpoint implements entity {
         mCommentGUID = guid;
         dateAcquired = System.currentTimeMillis();
         localStatus = AppDatabase.statusTypes.New;
+        mGuid = Math.random() + "";
     }
 
     public Viewpoint(String guid, String commentGUID, String type, String name,
@@ -100,8 +101,10 @@ public class Viewpoint implements entity {
     private void construct(JSONObject jsonObject) {
         try {
             mGuid = jsonObject.getString(GUID);
-            if (jsonObject.has(SNAPSHOT))
+            if (jsonObject.has(SNAPSHOT)) {
                 hasSnapshot = true;
+                mSnapshot = new Snapshot(SNAPSHOT_TYPE_PNG, mGuid);
+                }
         } catch (JSONException e) {
             e.printStackTrace();
         }
